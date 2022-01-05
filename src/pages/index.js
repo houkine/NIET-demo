43cff52c9e9a2d1fa7c101fe71c07c31b2439845
pan@konnectdigital.io <pan@konnectdigital.io>
2022-01-05 22:36:10 +1000

    first

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { mdiEmailOpenOutline } from '@mdi/js'
+
+import './index.css'
+const Home = () =>{
+    return(
+        <div className='Home-container'>
+            <div className='Home-line1'>
+            
+            </div>
+        </div>
+    )
+}
+
+export default Home
+
+const Header = () =>(
+    <div className='Home-container'>
+        <div className='Home-line1'>
+            <mdiEmailOpenOutline />
+        </div>
+    </div>
+)
